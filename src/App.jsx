d133e8c82f2c84542615b55a454f65e70db2865b
hishammpsn <hishammpsnhn@gmail.com>
2024-12-21 22:29:53 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <span data-testid ="span">{count}</span>
       <button onClick={() => setCount((prev)=>prev+2)}>Increment</button>
+      <p>Hello</p>
     </>
   );
 }
